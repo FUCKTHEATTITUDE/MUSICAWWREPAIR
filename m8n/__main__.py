51248ae7be919a69a43978483d44fa8eb2f032d8
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2022-06-10 07:53:05 +0530

    Update __main__.py

--- m8n/__main__.py
+++ m8n/__main__.py
@@ -42,8 +42,8 @@ async def load_start():
             pass
     await app.send_message(OWNER_ID, "**M8N Music Bot Started Successfully !!**")
    # Copyrighted Area
-    await client.join_chat("M8N_SUPPORT")
-    await client.join_chat("M8N_OFFICIAL")
+    await client.join_chat("Official_Devil_Support")
+    await client.join_chat("teamluna")
     print("[INFO]: STARTED")
     
 
